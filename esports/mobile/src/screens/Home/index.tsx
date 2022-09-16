613ddaa2772d81f48a1c608e9f39e5becfeb8727
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2022-09-16 12:51:49 -0300

    fetch back-end

--- esports/mobile/src/screens/Home/index.tsx
+++ esports/mobile/src/screens/Home/index.tsx
@@ -1,14 +1,22 @@
+import { useState, useEffect } from 'react';
 import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard';
 
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 
-import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch('http://192.168.0.5:3333/games') //machine ip
+    .then(response => response.json())
+    .then(data => setGames(data))
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image 
@@ -20,7 +28,7 @@ export function Home() {
         subtitle="Selecione o game que deseja jogar..."
       />
       <FlatList 
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
           <GameCard 
