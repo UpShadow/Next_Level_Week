f6eb3f3d4822a1f2f2ff538f39e32a5904c17e45
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2022-09-16 15:31:47 -0300

    navigation 50%

--- esports/mobile/src/screens/Home/index.tsx
+++ esports/mobile/src/screens/Home/index.tsx
@@ -1,15 +1,24 @@
 import { useState, useEffect } from 'react';
-import { View, Image, FlatList } from 'react-native';
+import { Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'; 
+import { useNavigation } from '@react-navigation/native'
 
 import logoImg from '../../assets/logo-nlw-esports.png'
 
 import { GameCard, GameCardProps } from '../../components/GameCard';
+import { Background } from '../../components/Background';
 import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
 export function Home() {
   const [games, setGames] = useState<GameCardProps[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleOpenGame() {
+    navigation.navigate('game');
+  }
 
   useEffect(() => {
     fetch('http://192.168.0.5:3333/games') //machine ip
@@ -18,29 +27,31 @@ export function Home() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Image 
-        source={logoImg}
-        style={styles.logo}
-      />
-      <Heading 
-        title="Encontre seu duo!"
-        subtitle="Selecione o game que deseja jogar..."
-      />
-      <FlatList 
-        data={games}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <GameCard 
-            data={item}
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image 
+          source={logoImg}
+          style={styles.logo}
+        />
+        <Heading 
+          title="Encontre seu duo!"
+          subtitle="Selecione o game que deseja jogar..."
+        />
+        <FlatList 
+          data={games}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <GameCard 
+              data={item}
+              onPress={handleOpenGame}
+            />
+          )}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.contentList}
+        />
         
-          />
-        )}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        contentContainerStyle={styles.contentList}
-      />
-      
-    </View>
+      </SafeAreaView>
+    </Background>
   );
 }
